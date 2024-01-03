35fc124adf573b52f61dab1aa33c0cece321d604
Thanasis Georgiadis <ageorgiadis@cs.uoi.gr>
2024-01-03 22:21:14 +0200

    added argument options and test case

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ $# -eq 0 ] ; then
     # default execution
     echo mpirun.mpich -np 10 ./program
     mpirun.mpich -np 10 ./program;
-elif [ $1 == test ] ; then
-    echo mpirun.mpich -np 10 ./program /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T1NA_fixed_binary.dat /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T2NA_fixed_binary.dat
-    mpirun.mpich -np 10 ./program ./program /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T1NA_fixed_binary.dat /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T2NA_fixed_binary.dat
+elif [ $1 == test1 ] ; then
+    echo "Full cluster Intersection Join between T1NA and T2NA using default APRIL (N=16, P=1, uncompressed)"
+    mpirun.mpich -np 10 ./program ./program -P /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T1NA_fixed_binary.dat /home/thanasis/Desktop/PhD/data_files/TIGER/binary/T2NA_fixed_binary.dat
 fi
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "env/comm_message.h"
 #include "env/disk_local_index.h"
+#include <bits/getopt_core.h>
 
 int main(int argc, char **argv)
 {
     // Initialize the MPI environment
+    char c;
     int provided;
 	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
 	MPI_Comm_size(MPI_COMM_WORLD, &WORLD_SIZE);
@@ -12,10 +14,58 @@ int main(int argc, char **argv)
 
     // printf("Hi from node %d\n", NODE_RANK);
     
-    std::string pathToFileR(argv[1]);
-    std::string pathToFileS(argv[2]);
+    // std::string argument1(argv[argc-2]);
+    // std::string argument2(argv[argc-1]);
 
     DiskIndexT diskIndex;
+
+    while ((c = getopt(argc, argv, "Pp:jwsraiN:h:c")) != -1)
+    {
+        switch (c)
+        {
+            case 'P':
+                // perform the partitioning (default 1000 parititons per dimension)
+                // printf("Partitioning of data requested\n");
+                break;
+            case 'p':
+                // # of partitions for grid per dimension
+                // printf("Will use %d partitions for the partitioning of the data\n", atoi(optarg));
+                break;   
+            case 'j':
+                // intersection join
+                break;   
+            case 'w':
+                // within join
+                break;
+            case 's':
+                // selection/range query
+                break;
+            case 'r':
+                // use refinement
+                break;
+            /* INTERMEDIATE FILTER */
+            case 'a':
+                // use APRIL in query
+                break;
+            case 'i':
+                // use RI in query
+                break;
+            /* APRIL and RI */
+            case 'N':
+                // set N for APRIL or RI (default N = 16)
+                break;
+            /* APRIL SPECIFIC */
+            case 'h':
+                // # of partitions for APRIL
+                break;
+            case 'c':
+                // use compressed APRIL
+                break;
+            
+            default:
+                break;
+        }
+    }
     
 
     DB_STATUS ret = SystemInit(DISK_SINGLE_MACHINE, &diskIndex);
